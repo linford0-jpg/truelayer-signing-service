c5b67892cf2edbaffb81727fda6050f4abf560dd
linford0-jpg <linford0@gmail.com>
2025-09-05 14:48:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { sign } = require('@truelayer/signing');
+const { sign } = require('truelayer-signing');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
